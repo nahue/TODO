91f764e7c81be3cdc6242964308f3ffa0a3545c0
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-03-07 14:58:08 -0300

    unicorn init script

--- provision/unicorn.sh
+++ provision/unicorn.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Installing Unicorn"
-sudo gem install unicorn >/dev/null 2>&1
+#echo "Installing Unicorn"
+#sudo gem install unicorn >/dev/null 2>&1
+echo "Setup init script"
+sudo ln -s /home/vagrant/deploy/TODO_staging/current/provision/unicorn_init.sh /etc/init.d/unicorn
 echo "Unicorn installed"
