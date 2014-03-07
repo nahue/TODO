8ed0be010bd2dbccf49736ccdeb130531fa16044
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-03-07 16:47:34 -0300

    app scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ TODO::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile =  ['*.js', '*.scss', '*.css.erb']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
