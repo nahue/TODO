2d75db6d34fb16d52f4799ee485e1765174b8aff
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-03-07 16:11:12 -0300

    unicorn

--- provision/nginx.sh
+++ provision/nginx.sh
@@ -3,6 +3,7 @@
 echo "Installing Nginx"
 apt-get install -y nginx >/dev/null 2>&1
 rm -f                               /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/rails_starter
 ln -s /vagrant/provision/nginx.conf /etc/nginx/sites-enabled/rails_starter
 service nginx start
 echo "Nginx installed"
--- provision/unicorn.sh
+++ provision/unicorn.sh
@@ -3,5 +3,6 @@
 #echo "Installing Unicorn"
 #sudo gem install unicorn >/dev/null 2>&1
 echo "Setup init script"
+sudo rm /etc/init.d/unicorn
 sudo ln -s /home/vagrant/deploy/TODO_staging/current/provision/unicorn_init.sh /etc/init.d/unicorn
 echo "Unicorn installed"
